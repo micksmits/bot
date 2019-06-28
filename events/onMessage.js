bc597c9404c5ce3564b5fe58ecbcf42f834fcd18
Mick Smits <micksmits96@gmail.com>
2019-06-29 01:17:46 +0200

    Round 4 bepis

--- events/onMessage.js
+++ events/onMessage.js
@@ -9,6 +9,9 @@ module.exports = class
     async run (message) {
         if (message.channel.name === 'selfies' && message.attachments.size < 1) message.delete();
 
+        if (message.content.includes('В') && message.channel.name !== 'logs') message.delete();
+
+
         if (message.content.toLowerCase().replace(/\s/g, '').includes('bepis') && message.channel.name !== 'logs') message.delete();
     }
 }
