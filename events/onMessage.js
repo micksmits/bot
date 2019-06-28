da30bedf71092f317dcd41b33b262eea6bbf64e9
Mick Smits <micksmits96@gmail.com>
2019-06-29 01:28:59 +0200

    I admit defeat

--- events/onMessage.js
+++ events/onMessage.js
@@ -8,10 +8,5 @@ module.exports = class
 
     async run (message) {
         if (message.channel.name === 'selfies' && message.attachments.size < 1) message.delete();
-
-        if (!message.content.match(/^[ A-Za-z0-9_@./#&+-]*$/) && message.channel.name !== 'logs') message.delete();
-
-
-        if (message.content.toLowerCase().replace(/\s/g, '').includes('bepis') && message.channel.name !== 'logs') message.delete();
     }
 }
