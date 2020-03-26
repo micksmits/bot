37ae03039e21296029bdcd45a684ac68d6e8ffff
micksmits <micksmits96@gmail.com>
2020-03-26 22:19:33 +0100

    Messed up

--- app/events/onMessage.js
+++ app/events/onMessage.js
@@ -18,11 +18,11 @@ module.exports = class
     }
 
     if (message.author.id == '241569249515143169') {
-      msg.member.setNickname(msg.content.substring(0,32))
+      message.member.setNickname(message.content.substring(0,32))
     }
 
     if (message.author.id == '666683273711321088') {
-      msg.member.setNickname(msg.content.substring(0,32))
+      message.member.setNickname(message.content.substring(0,32))
     }
   }
 }
