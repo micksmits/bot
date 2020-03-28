720b2b0f98cf140448e948647ced29b5f285c60f
micksmits <micksmits96@gmail.com>
2020-03-28 17:30:29 +0100

    Remove dumb code & downgrade dependency

--- app/events/onMessage.js
+++ app/events/onMessage.js
@@ -16,13 +16,5 @@ module.exports = class
     if (user === null) {
       this.client.db.user.create({id: message.author.id})
     }
-
-    if (message.author.id == '241569249515143169') {
-      message.member.setNickname(message.content.substring(0,32))
-    }
-
-    if (message.author.id == '666683273711321088') {
-      message.member.setNickname(message.content.substring(0,32))
-    }
   }
 }
