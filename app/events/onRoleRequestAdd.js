d7114d0e284127f51110de6664d4ddd6c2adad6a
micksmits <micksmits96@gmail.com>
2020-05-05 18:13:44 +0200

    Only get reactions if message is present

--- app/events/onRoleRequestAdd.js
+++ app/events/onRoleRequestAdd.js
@@ -9,14 +9,16 @@ module.exports = class
   async run (reaction, user) {
     const message = reaction.message
     const rolemessage = await this.client.db.message.findByPk(message.id)
-    const reactions = await rolemessage.getReactions()
+    if (rolemessage) {
+      const reactions = await rolemessage.getReactions()
 
-    reactions.forEach(async reactionRecord => {
-      if (reaction.emoji.id == reactionRecord.id) {
-        message.guild.member(user).addRole(reactionRecord.role_id)
-      } else {
-        await reaction.remove(user)
-      }
-    })
+      reactions.forEach(async reactionRecord => {
+        if (reaction.emoji.id == reactionRecord.id) {
+          message.guild.member(user).addRole(reactionRecord.role_id)
+        } else {
+          await reaction.remove(user)
+        }
+      })
+    }
   }
 }
--- app/events/onRoleRequestRemove.js
+++ app/events/onRoleRequestRemove.js
@@ -9,12 +9,14 @@ module.exports = class
   async run (reaction, user) {
     const message = reaction.message
     const rolemessage = await this.client.db.message.findByPk(message.id)
-    const reactions = await rolemessage.getReactions()
+    if (rolemessage) {
+      const reactions = await rolemessage.getReactions()
 
-    reactions.forEach(reactionRecord => {
-      if (reaction.emoji.id == reactionRecord.id) {
-        message.guild.member(user).removeRole(reactionRecord.role_id)
-      }
-    })
+      reactions.forEach(reactionRecord => {
+        if (reaction.emoji.id == reactionRecord.id) {
+          message.guild.member(user).removeRole(reactionRecord.role_id)
+        }
+      })
+    }
   }
 }
