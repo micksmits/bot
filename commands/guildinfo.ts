3eb89bda8058ea3d7ac5e393b3d805e95007b526
lhyrai <1997nickyjansen@gmail.com>
2023-12-11 10:46:23 -0800

    implementing guild info feat

--- commands/guildinfo.ts
+++ commands/guildinfo.ts
@@ -13,16 +13,41 @@ export default {
 
     
     async execute(interaction: any) {
-        const result = await axios.get('https://api.guildwars2.com/v2/guild/'), {
-            params: {
-                ID: process.env.GUILD_ID,
-                headers: {
-                    'Authorization': `bearer ${process.env.GW2_API}`
-                }
-            }
-        }
-        
+        const gw2Result = await axios.get(`https://api.guildwars2.com/v2/guild/${process.env.GW2_GUILD_ID}`, {
+            headers: {
+                'Authorization': `Bearer ${process.env.GW2_API}`
+            },
+        })
+        console.log(gw2Result)
+        const embed = new EmbedBuilder()
+            .setColor('#b700ff')
+            .setTitle(`${gw2Result.data.name}`)
+            .addFields(
+                {
+                  name: "Influence",
+                  value: `Our guild currently has ${gw2Result.data.influence} influence`,
+                  inline: false
+                },
+                {
+                  name: "Aetherium",
+                  value: `Our guild currently has ${gw2Result.data.aetherium} aetherium`,
+                  inline: false
+                },
+                {
+                  name: "Favor",
+                  value: `Our guild currently has ${gw2Result.data.favor} favor`,
+                  inline: false
+                },
+                {
+                  name: "Current amount of guild members",
+                  value: `Our guild currently has ${gw2Result.data.member_count} members`,
+                  inline: false
+                },
+              )
+        await interaction.reply({ embeds: [embed] });
     }
+
+    
 
 
 }
