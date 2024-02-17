c605567802a738269825bd604e2c319824dcdb83
Mick <micksmits96@gmail.com>
2024-02-17 18:56:03 +0100

    chore: remove ping cmd

--- commands/ping.ts
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export default {
-  data: new SlashCommandBuilder().setName('ping').setDescription('Send a pong back'),
-  async execute(interaction: any) {
-    await interaction.reply('Pong!');
-  },
-};
